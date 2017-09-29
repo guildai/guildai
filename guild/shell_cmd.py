ae6c20e195a1412f4978376c5ef571aba8542b76
Garrett Smith <g@rre.tt>
2017-09-29 14:46:06 -0500

    Init guild app to start shell session

--- guild/shell_cmd.py
+++ guild/shell_cmd.py
@@ -6,5 +6,7 @@ def main(_args):
     env = {
         "PYTHONPATH": os.path.pathsep.join(sys.path)
     }
-    p = subprocess.Popen([sys.executable, "-Ss"], env=env)
+    p = subprocess.Popen(
+        [sys.executable, "-Ssic", "import guild.app;guild.app.init()"],
+        env=env)
     p.communicate()
