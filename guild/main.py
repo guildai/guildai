38e4dc2b46dcf814f8e7a1d96812ad695e6180ac
Garrett Smith <g@rre.tt>
2017-10-03 10:14:37 -0500

    train cmd alias

--- guild/main.py
+++ guild/main.py
@@ -414,3 +414,47 @@ def remove_source(**kw):
     guild.sources_cmd.remove_source(Args(kw))
 
 sources.add_command(remove_source)
+
+###################################################################
+# train command
+###################################################################
+
+@click.command("train")
+@click.argument("model", required=False)
+@click.argument("args", metavar="[ARG...]", nargs=-1)
+@click.option(
+    "-p", "--project", "project_location",
+    help="Project location (file system directory) for MODEL.",
+    metavar="LOCATION"
+)
+@click.option(
+    "-y", "--yes",
+    help="Do not prompt before running operation.",
+    is_flag=True)
+
+def train(**kw):
+    """Train a model.
+
+    Equivalent to running 'guild run [MODEL:]train [ARG...]'.
+
+    By default MODEL is the default model for project in LOCATION.
+
+    You may omit MODEL (i.e. for training the default model) while
+    providing one or more ARG values provided the first ARG value
+    contains an equals sign ('='). When specifying a boolean flag in
+    such cases, use 'NAME=true'.
+
+    Refer to help for the run command ('guild run --help') for more
+    information.
+    """
+    import guild.run_cmd
+    args = Args(kw)
+    # MODEL is treated as an ARG if it contains an equal sign (see
+    # help text above).
+    if args.model and "=" in args.model:
+        args.args = (args.model,) + args.args
+        args.model = None
+    args.opspec = "%s:train" % args.model if args.model else "train"
+    guild.run_cmd.main(args)
+
+cli.add_command(train)
