d463374b4109b226329c2bada5ffbcc769fe78e8
Garrett Smith <g@rre.tt>
2017-10-06 09:04:40 -0500

    Ensure deterministic order of models for plugin

--- guild/plugins/keras.py
+++ guild/plugins/keras.py
@@ -4,11 +4,11 @@ from guild.plugins import python_util
 class KerasPlugin(Plugin):
 
     def models_for_location(self, path):
-        for script in _keras_scripts(path):
+        for script in _sorted_keras_scripts(path):
             yield _script_model(script)
 
-def _keras_scripts(path):
-    for script in python_util.scripts_for_location(path):
+def _sorted_keras_scripts(path):
+    for script in sorted(python_util.scripts_for_location(path)):
         if _is_keras_script(script):
             yield script
 
