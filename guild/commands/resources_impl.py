862db7c08ad676e5f8f4d6727cde7a6615d25757
Garrett Smith <g@rre.tt>
2017-10-31 06:52:05 -0500

    Actually filter resources in list

--- guild/commands/resources_impl.py
+++ guild/commands/resources_impl.py
@@ -18,12 +18,14 @@ from __future__ import division
 from guild import cli
 from guild import resource
 from guild import cmd_impl_support
+from guild import util
 
 def main(args):
     cmd_impl_support.init_resource_path(args.all, "--all")
     formatted = [_format_resource(r) for r in resource.iter_resources()]
+    filtered = [r for r in formatted if _filter_resource(r, args)]
     cli.table(
-        sorted(formatted, key=lambda r: r["name"]),
+        sorted(filtered, key=lambda r: r["name"]),
         cols=["name", "description"],
         detail=(["sources"] if args.verbose else [])
     )
@@ -34,3 +36,10 @@ def _format_resource(res):
         "description": res.resdef.description or "",
         "sources": [str(s) for s in res.resdef.sources],
     }
+
+def _filter_resource(res, args):
+    filter_vals = [
+        res["name"],
+        res["description"],
+    ]
+    return util.match_filter(args.filters, filter_vals)
