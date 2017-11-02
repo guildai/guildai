c2ef3611f97f1691bb15bc6b4a936a7542b129d1
Garrett Smith <g@rre.tt>
2017-11-02 11:59:27 -0500

    Docs UI tweaks

--- docs/conf.py
+++ docs/conf.py
@@ -1,22 +1,22 @@
 import sphinx_rtd_theme
 
 project = u'Guild AI'
-copyright = u'2017, TensorHub, Inc'
+copyright = u'2017, TensorHub, Inc.'
 version = u'0.2.0.dev4'
 author = u'Guild AI'
 
 master_doc = 'index'
 html_theme = 'sphinx_rtd_theme'
 html_context = {
-    "show_sphinx": False,
-    "commit": False,
+    "show_source": False,
 }
+templates_path = ['templates']
+exclude_patterns = ['build']
 
 # The following are temporarily disabled until the docs fill in. Will
 # cleanup in time.
 
 """
-templates_path = ['_templates']
 extensions = ['sphinx.ext.todo',
     'sphinx.ext.coverage',
     'sphinx.ext.ifconfig',
@@ -29,7 +29,6 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 pygments_style = 'sphinx'
 todo_include_todos = True
 html_theme_options = {}
-html_static_path = ['_static']
 html_sidebars = {
     '**': [
         'relations.html',  # needs 'show_related': True theme option to display
