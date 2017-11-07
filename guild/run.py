ca53bb3ca3953ab80f164d867c48e175031414c0
Garrett Smith <g@rre.tt>
2017-11-07 12:08:49 -0600

    Avoid unused optimizations

--- guild/run.py
+++ guild/run.py
@@ -36,33 +36,42 @@ class Run(object):
 
     def __init__(self, id, path):
         self.id = id
+        self.short_id = id[:8]
         self.path = path
         self._guild_dir = os.path.join(self.path, ".guild")
-        self._pid = None
-        self._status = None
-        self._extended_status = None
 
-    @property
-    def short_id(self):
-        return self.id[:8]
-
-    @property
     def pid(self):
-        if self._pid is None:
-            self._pid = _op_pid(self)
-        return self._pid
+        lockfile = self.guild_path("LOCK")
+        try:
+            raw = open(lockfile, "r").read()
+        except (IOError, ValueError):
+            return None
+        else:
+            return int(raw)
 
-    @property
     def status(self):
-        if self._status is None:
-            self._status = _op_status(self)
-        return self._status
+        pid = self.pid()
+        if pid is None:
+            return "stopped"
+        elif guild.util.pid_exists(pid):
+            return "running"
+        else:
+            return "crashed"
 
-    @property
     def extended_status(self):
-        if self._extended_status is None:
-            self._extended_status = _extended_op_status(self)
-        return self._extended_status
+        """Uses exit_status to extend the status to include error or success."""
+        base_status = self.status()
+        if base_status == "running":
+            return "running"
+        elif base_status == "crashed":
+            return "terminated"
+        elif base_status == "stopped":
+            if self.get("exit_status") == 0:
+                return "completed"
+            else:
+                return "error"
+        else:
+            raise AssertionError(base_status)
 
     def get(self, name, default=None):
         try:
@@ -98,20 +107,6 @@ class Run(object):
         guild.util.ensure_dir(self.guild_path("attrs"))
         guild.util.ensure_dir(self.guild_path("logs"))
 
-    def update_status(self):
-        """Update status and extended_status.
-
-        The attribute status and extended_status are expensive to read
-        (OS side effects) and so are read lazily and cached. Use this
-        method to update these attributes to the latest value.
-
-        (Note that calling this method merely invalidates the cached
-        values - new values are read lazily.)
-        """
-        self._pid = None
-        self._status = None
-        self._extended_status = None
-
     def guild_path(self, subpath):
         return os.path.join(self._guild_dir, subpath)
 
@@ -138,39 +133,6 @@ class Run(object):
                     yield os.path.join(root, name)
             for name in files:
                 yield os.path.join(root, name)
-
-def _op_pid(run):
-    lockfile = run.guild_path("LOCK")
-    try:
-        raw = open(lockfile, "r").read()
-    except (IOError, ValueError):
-        return None
-    else:
-        return int(raw)
-
-def _op_status(run):
-    pid = run.pid
-    if pid is None:
-        return "stopped"
-    elif guild.util.pid_exists(pid):
-        return "running"
-    else:
-        return "crashed"
-
-def _extended_op_status(run):
-    """Uses exit_status to extend the status to include error or success."""
-    base_status = run.status
-    if base_status == "running":
-        return "running"
-    elif base_status == "crashed":
-        return "terminated"
-    elif base_status == "stopped":
-        if run.get("exit_status") == 0:
-            return "completed"
-        else:
-            return "error"
-    else:
-        raise AssertionError(base_status)
 
 def timestamp():
     """Returns an integer use for run timestamps."""
