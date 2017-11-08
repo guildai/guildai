1d7f7fcc2e4af622b35d5de595f2b4f52664a987
Garrett Smith <g@rre.tt>
2017-11-07 18:05:46 -0600

    Support properties-as-method-calls
    This is a bit weird. We could just implement these as properties as
    before with cached values, but this is all a lot of thinking and hand
    wringing.

--- guild/var.py
+++ guild/var.py
@@ -118,7 +118,8 @@ def _run_attr_cmp(x, y, attr):
 
 def _run_attr(run, name):
     if name in guild.run.Run.__properties__:
-        return getattr(run, name)
+        val = getattr(run, name)
+        return val() if callable(val) else val
     else:
         return run.get(name)
 
