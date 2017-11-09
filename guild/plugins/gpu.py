6fa56005f54ad0043d1ccd6cc91c619bfc223c20
Garrett Smith <g@rre.tt>
2017-11-09 06:15:23 -0600

    Fix GPU plugin logging

--- guild/plugins/gpu.py
+++ guild/plugins/gpu.py
@@ -17,7 +17,6 @@ from __future__ import division
 
 import csv
 import io
-import logging
 import subprocess
 import sys
 
@@ -49,7 +48,42 @@ class GPUPlugin(SummaryPlugin):
         return True, ""
 
     def read_summary_values(self):
-        return _gpu_stats(self._stats_cmd) if self._stats_cmd else {}
+        return self._gpu_stats(self._stats_cmd) if self._stats_cmd else {}
+
+    def _gpu_stats(self, stats_cmd):
+        stats = {}
+        for raw in self._read_raw_gpu_stats(stats_cmd):
+            stats.update(self._calc_gpu_stats(raw))
+        return stats
+
+    def _read_raw_gpu_stats(self, stats_cmd):
+        p = subprocess.Popen(stats_cmd, stdout=subprocess.PIPE)
+        raw_lines = _read_csv_lines(p.stdout)
+        result = p.wait()
+        if result == 0:
+            return raw_lines
+        else:
+            self.log.error("reading GPU stats (smi output: '%s')", raw_lines)
+            return []
+
+    @staticmethod
+    def _calc_gpu_stats(raw):
+        # See STATS for list of stat names/indexes
+        index = raw[0]
+        mem_total = _parse_raw(raw[3], _parse_bytes)
+        mem_used = _parse_raw(raw[4], _parse_bytes)
+        vals = [
+            ("fanspeed", _parse_raw(raw[1], _parse_percent)),
+            ("pstate", _parse_raw(raw[2], _parse_pstate)),
+            ("mem_total", mem_total),
+            ("mem_used", mem_used),
+            ("mem_free", mem_total - mem_used),
+            ("mem_util", _parse_raw(raw[6], _parse_percent)),
+            ("util", _parse_raw(raw[5], _parse_percent)),
+            ("temp", _parse_raw(raw[7], _parse_int)),
+            ("powerdraw", _parse_raw(raw[8], _parse_watts))
+        ]
+        return dict([(_gpu_val_key(index, name), val) for name, val in vals])
 
 def _stats_cmd():
     try:
@@ -63,43 +97,10 @@ def _stats_cmd():
              "--query-gpu=%s" % ",".join(STATS),
         ]
 
-def _gpu_stats(stats_cmd):
-    stats = {}
-    for raw in _read_raw_gpu_stats(stats_cmd):
-        stats.update(_calc_gpu_stats(raw))
-    return stats
-
-def _read_raw_gpu_stats(stats_cmd):
-    p = subprocess.Popen(stats_cmd, stdout=subprocess.PIPE)
-    raw_lines = _read_csv_lines(p.stdout)
-    result = p.wait()
-    if result == 0:
-        return raw_lines
-    else:
-        logging.error("reading GPU stats (smi output: '%s')", raw_lines)
-        return []
 
 def _read_csv_lines(raw_in):
     csv_in = raw_in if sys.version_info[0] == 2 else io.TextIOWrapper(raw_in)
     return list(csv.reader(csv_in))
-
-def _calc_gpu_stats(raw):
-    # See STATS for list of stat names/indexes
-    index = raw[0]
-    mem_total = _parse_raw(raw[3], _parse_bytes)
-    mem_used = _parse_raw(raw[4], _parse_bytes)
-    vals = [
-        ("fanspeed", _parse_raw(raw[1], _parse_percent)),
-        ("pstate", _parse_raw(raw[2], _parse_pstate)),
-        ("mem_total", mem_total),
-        ("mem_used", mem_used),
-        ("mem_free", mem_total - mem_used),
-        ("mem_util", _parse_raw(raw[6], _parse_percent)),
-        ("util", _parse_raw(raw[5], _parse_percent)),
-        ("temp", _parse_raw(raw[7], _parse_int)),
-        ("powerdraw", _parse_raw(raw[8], _parse_watts))
-    ]
-    return dict([(_gpu_val_key(index, name), val) for name, val in vals])
 
 def _parse_raw(raw, parser):
     stripped = raw.strip()
