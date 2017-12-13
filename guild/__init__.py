89a6f517b1b744b5c0b3ea6eb382bc92cdf04bdd
Garrett Smith <g@rre.tt>
2017-12-13 11:42:35 -0600

    Fix pre-release version
    'pre' is changed by setuptools to 'rc', which is not where we are atm.

--- guild/__init__.py
+++ guild/__init__.py
@@ -18,7 +18,7 @@ from __future__ import division
 import os
 import subprocess
 
-__version__ = "0.3.0.pre1"
+__version__ = "0.3.0.dev1"
 
 __requires__ = [
     # (<required module>, <distutils package req>)
