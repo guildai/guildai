5eea52ccd78efd45cc3bf09b121cff5131f7d8fa
Garrett Smith <g@rre.tt>
2018-01-03 14:33:02 -0600

    Runs list filter and stub for TensorBoard link

--- guild/view/src/components/guild-runs-list.vue
+++ guild/view/src/components/guild-runs-list.vue
@@ -1,15 +1,43 @@
 <template>
   <v-list>
-    <v-subheader>Runs</v-subheader>
+    <v-subheader>
+      <div>Runs</div>
+      <v-spacer />
+      <v-tooltip right transition="fade-transition">
+        <v-btn
+          flat small
+          slot="activator"
+          @click="tensorboard">
+          &ensp;View in TensorBoard
+        </v-btn>
+        <span>View in TensorBoard</span>
+      </v-tooltip>
+    </v-subheader>
+    <v-list-tile>
+      <div style="width:100%; margin-top:-24px">
+        <v-text-field
+          v-model="filter"
+          append-icon="search"
+          label="Filter"
+          single-line
+          hide-details
+          clearable
+          style="font-weight:400"
+        />
+      </div>
+    </v-list-tile>
     <v-divider />
-    <template v-for="run in runs">
+    <template v-for="run in filtered">
       <v-list-tile
         :key="run.id"
         @click="runSelected(run)"
         :class="{selected: value.id === run.id}"
         ripple>
         <v-list-tile-content>
-          <v-tooltip top transition="fade-transition" tag="div" style="width:100%">
+          <v-tooltip
+            top transition="fade-transition"
+            tag="div"
+            class="rev-ellipsis-container">
             <v-list-tile-title slot="activator" class="rev-ellipsis">
               &lrm;{{ run.operation }}
             </v-list-tile-title>
@@ -35,9 +63,30 @@
      value: { type: Object }
    },
 
+   data() {
+     return {
+       filter: ''
+     };
+   },
+
+   computed: {
+     filtered() {
+       if (!this.filter) {
+         return this.runs;
+       }
+       const filter = this.filter.toLowerCase();
+       return this.runs.filter(
+         run => run.operation.toLowerCase().indexOf(filter) !== -1);
+     }
+   },
+
    methods: {
      runSelected(run) {
        this.$emit('input', run);
+     },
+
+     tensorboard() {
+       console.log('TODO: open TensorBoard in another tab');
      }
    }
  };
@@ -47,6 +96,10 @@
  .list__tile {
    height: inherit;
    padding: 10px 16px;
+ }
+
+ .rev-ellipsis-container {
+   width: 100%;
  }
 
  .rev-ellipsis {
