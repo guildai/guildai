cafda2ccd8933d3e4b7a5605c95a2328d296b610
Garrett Smith <g@rre.tt>
2018-01-25 18:23:06 -0600

    Bump version

--- guild/__init__.py
+++ guild/__init__.py
@@ -18,7 +18,7 @@ from __future__ import division
 import os
 import subprocess
 
-__version__ = "0.3.0.dev2"
+__version__ = "0.3.0.dev4"
 
 __requires__ = [
     # (<required module>, <distutils package req>)
