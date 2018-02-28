f273c15acbc527330a73377a433a98f266ebb556
Garrett Smith <g@rre.tt>
2018-02-28 14:48:12 -0600

    Reinstate all builds

--- .circleci/config.py
+++ .circleci/config.py
@@ -228,11 +228,11 @@ class Config(object):
         }
 
 builds = [
-    #LinuxBuild(python="2.7"),
-    #LinuxBuild(python="3.5"),
-    #LinuxBuild(python="3.6"),
+    LinuxBuild(python="2.7"),
+    LinuxBuild(python="3.5"),
+    LinuxBuild(python="3.6"),
     MacBuild(python="2.7"),
-    #MacBuild(python="3.6"),
+    MacBuild(python="3.6"),
 ]
 
 def main():
