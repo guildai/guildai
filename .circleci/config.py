d4bfcabc309c33adc01fac7f9d6775840abb1480
Garrett Smith <g@rre.tt>
2018-03-05 06:08:00 -0600

    Update copyright for 2018

--- .circleci/config.py
+++ .circleci/config.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-# Copyright 2017 TensorHub, Inc.
+# Copyright 2017-2018 TensorHub, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
