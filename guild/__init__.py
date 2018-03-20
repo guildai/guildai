c82f257ebb71d2d0915d21a550d2b933b05673c3
Garrett Smith <g@rre.tt>
2018-03-20 11:23:25 -0500

    0.3.1.dev3

--- guild/__init__.py
+++ guild/__init__.py
@@ -18,7 +18,7 @@ from __future__ import division
 import os
 import subprocess
 
-__version__ = "0.3.1.dev2"
+__version__ = "0.3.1.dev3"
 
 __requires__ = [
     # (<required module>, <distutils package req>)
