5ada147cdd914b0e825001c3a37cbe1aebfe6196
Garrett Smith <g@rre.tt>
2018-08-23 11:41:24 -0500

    Use new service support to implement shutdown timer

--- guild/commands/shutdown_timer_impl.py
+++ guild/commands/shutdown_timer_impl.py
@@ -15,60 +15,35 @@
 from __future__ import absolute_import
 from __future__ import division
 
-import logging
-import os
 import subprocess
 import time
 
-import daemonize
 import psutil
 
-from guild import cli
-from guild import util
-from guild import var
+from . import service_impl_support
+
+NAME = "shutdown-timer"
+TITLE = "Shutdown timer"
 
 INTERVAL = 5
-LOG_MAX_SIZE = 102400
-LOG_BACKUPS = 1
 LOG_HEARTBEAT = 60
 
 def start(args):
-    log = _init_log(args)
-    log.info("Shutdown timer started with timeout = %im", args.timeout)
+    run = lambda log: _run(args, log)
+    service_impl_support.start(NAME, run, args, TITLE)
+
+def stop():
+    service_impl_support.stop(NAME, TITLE)
+
+def status():
+    service_impl_support.status(NAME, TITLE)
+
+def _run(args, log):
+    log.info("%s started with timeout = %im", TITLE, args.timeout)
     if args.dont_shutdown:
-        cli.out(
-            cli.style(
-                "Shutdown timer WILL NOT SHUTDOWN system because "
-                "--dont-shutdown was used",
-                bold=True))
-    if args.foreground:
-        _run(args, log)
-    else:
-        _start(args, log)
-
-def _init_log(args):
-    if args.foreground:
-        handler = logging.StreamHandler()
-    else:
-        logfile = var.logfile("shutdown-timer")
-        util.ensure_dir(os.path.dirname(logfile))
-        handler = logging.handlers.RotatingFileHandler(
-            logfile,
-            maxBytes=LOG_MAX_SIZE,
-            backupCount=LOG_BACKUPS)
-    handler.setFormatter(logging.Formatter(
-        "%(asctime)s %(message)s",
-        "%Y-%m-%d %H:%M:%S"))
-    log = logging.getLogger("guild.shutdown_timer")
-    log.propagate = False
-    log.addHandler(handler)
-    return log
-
-def _run(args, log, log_level=None):
-    # When daemonized, log.level is 10 for some reason - reset to
-    # log_level if provided.
-    if log_level is not None:
-        log.setLevel(log_level)
+        log.info(
+            "%s WILL NOT SHUTDOWN system because "
+            "--dont-shutdown was used", TITLE)
     last_activity = _now()
     while True:
         last_activity = _check_activity(last_activity, log)
@@ -76,7 +51,7 @@ def _run(args, log, log_level=None):
             _shutdown(args, log)
             break
         time.sleep(INTERVAL)
-    log.info("Quitting")
+    log.info("Stopping")
 
 def _check_activity(last_activity, log):
     pids = _guild_ops()
@@ -142,82 +117,3 @@ def _shutdown(args, log):
         subprocess.check_call(cmd)
     except Exception:
         log.exception("shutdown")
-
-def _start(args, log):
-    pidfile = var.pidfile("shutdown-timer")
-    if os.path.exists(pidfile):
-        cli.error(
-            "shutdown timer is already running "
-            "(use 'guild sys shutdown-timer status' to verify)")
-    util.ensure_dir(os.path.dirname(pidfile))
-    # Save original log level to workaround issue with daemonization
-    # (see note in _run).
-    log_level = log.getEffectiveLevel()
-    daemon = daemonize.Daemonize(
-        app="guild",
-        action=lambda: _run(args, log, log_level),
-        pid=pidfile,
-        keep_fds=_log_fds(log))
-    cli.out(
-        "Starting shutdown timer "
-        "(use 'guild sys shutdown-timer stop' to stop)")
-    daemon.start()
-
-def _log_fds(log):
-    return [h.stream.fileno() for h in log.handlers if hasattr(h, "stream")]
-
-def stop():
-    pidfile = var.pidfile("shutdown-timer")
-    if not os.path.exists(pidfile):
-        cli.out("Shutdown timer is not running")
-        return
-    try:
-        pid = _read_pid(pidfile)
-    except Exception:
-        cli.out(
-            "Shutdown timer has an invalid pidfile (%s) - deleting"
-            % pidfile)
-        _delete_pidfile(pidfile)
-    else:
-        try:
-            proc = psutil.Process(pid)
-        except psutil.NoSuchProcess:
-            cli.out("Shutdown timer did not shut down cleanly - cleaning up")
-            _delete_pidfile(pidfile)
-        else:
-            cli.out("Stopping shutdown timer (pid %i)" % proc.pid)
-            proc.terminate()
-
-def _delete_pidfile(pidfile):
-    try:
-        os.remove(pidfile)
-    except OSError:
-        cli.error(
-            "Error deleting pidfile %s: %s\n"
-            "Try deleting the file manually."
-            % pidfile)
-
-def status():
-    pidfile = var.pidfile("shutdown-timer")
-    if os.path.exists(pidfile):
-        try:
-            pid = _read_pid(pidfile)
-        except Exception as e:
-            cli.error("error reading pid in %s: %s" % (pidfile, e))
-        else:
-            try:
-                proc = psutil.Process(pid)
-            except psutil.NoSuchProcess:
-                cli.out(
-                    "Shutdown timer is NOT running "
-                    "(orphaned pid in %s - use 'guild sys shutdown-timer stop' "
-                    "to cleanup)"
-                    % pidfile)
-            else:
-                cli.out("Shutdown timer is running (pid %i)" % proc.pid)
-    else:
-        cli.out("Shutdown timer is not running")
-
-def _read_pid(pidfile):
-    raw = open(pidfile, "r").read()
-    return int(raw.strip())
