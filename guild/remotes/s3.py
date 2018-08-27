5ef3d3157f4a3af5c3f0b36fa7029c75980088eb
Garrett Smith <g@rre.tt>
2018-08-27 10:30:03 -0500

    Support for deleting runs on s3

--- guild/remotes/s3.py
+++ guild/remotes/s3.py
@@ -16,6 +16,7 @@ from __future__ import absolute_import
 from __future__ import division
 
 import hashlib
+import itertools
 import logging
 import os
 import subprocess
@@ -30,23 +31,31 @@ from guild.commands import runs_impl
 
 log = logging.getLogger("guild.remotes.s3")
 
+RUNS_PATH = ["runs"]
+DELETED_RUNS_PATH = ["trash", "runs"]
+
 class S3Remote(remotelib.Remote):
 
     def __init__(self, name, config):
         self.name = name
-        self.bucket = bucket = config["bucket"]
-        self.root = root = config.get("root", "/")
+        self.bucket = config["bucket"]
+        self.root = config.get("root", "/")
         self.region = config.get("region")
-        self.local_sync_dir = lsd = self._local_sync_dir(name, bucket, root)
-        self._runs_dir = os.path.join(lsd, "runs")
-        self._deleted_runs_dir = os.path.join(lsd, "trash", "runs")
+        self.local_sync_dir = lsd = self._local_sync_dir()
+        self._runs_dir = os.path.join(lsd, *RUNS_PATH)
+        self._deleted_runs_dir = os.path.join(lsd, *DELETED_RUNS_PATH)
 
-    @staticmethod
-    def _local_sync_dir(name, bucket, root):
-        base_dir = var.remote_dir(name)
-        full_src = _s3_path_join(bucket, root)
-        uri_hash = hashlib.md5(full_src.encode()).hexdigest()
+    def _local_sync_dir(self):
+        base_dir = var.remote_dir(self.name)
+        uri_hash = hashlib.md5(self._s3_uri().encode()).hexdigest()
         return os.path.join(base_dir, "meta", uri_hash)
+
+    def _s3_uri(self, *subpath):
+        path = [
+            part for part in itertools.chain([self.root], subpath)
+            if part not in ("/", "")]
+        joined_path = "/".join(path)
+        return "s3://%s/%s" % (self.bucket, joined_path)
 
     def list_runs(self, verbose, **filters):
         self._verify_creds_and_region()
@@ -56,6 +65,7 @@ class S3Remote(remotelib.Remote):
             return
         args = click_util.Args(verbose=verbose, **filters)
         args.archive = runs_dir
+        args.deleted = False
         args.remote = False
         runs_impl.list_runs(args)
 
@@ -72,16 +82,15 @@ class S3Remote(remotelib.Remote):
             remote_util.require_env("AWS_DEFAULT_REGION")
 
     def _sync_runs_meta(self):
-        s3_path = _s3_path_join(self.bucket, self.root)
-        args = [
-            "s3://%s" % s3_path,
+        sync_args = [
+            self._s3_uri(),
             self.local_sync_dir,
             "--exclude", "*",
             "--include", "*/.guild/*",
             "--delete",
         ]
-        log.info("Synchrozing runs with %s", s3_path)
-        self._s3_cmd("sync", args, to_stderr=True)
+        log.info("Synchronizing runs with %s", self.name)
+        self._s3_cmd("sync", sync_args, to_stderr=True)
 
     def _s3api_cmd(self, name, args):
         cmd = ["aws"]
@@ -108,6 +117,57 @@ class S3Remote(remotelib.Remote):
         except subprocess.CalledProcessError as e:
             raise remotelib.RemoteProcessError.from_called_process_error(e)
 
+    def delete_runs(self, **opts):
+        self._verify_creds_and_region()
+        self._sync_runs_meta()
+        args = click_util.Args(**opts)
+        args.archive = self._runs_dir
+        if args.permanent:
+            preview = (
+                "WARNING: You are about to permanently delete "
+                "the following runs from %s:" % self.name)
+            confirm = "Permanently delete these runs?"
+        else:
+            preview = (
+                "You are about to delete the following runs from %s:"
+                % self.name)
+            confirm = "Delete these runs?"
+        no_runs_help = "Nothing to delete."
+        def delete_f(selected):
+            self._delete_runs(selected, args.permanent)
+        try:
+            runs_impl._runs_op(
+                args, None, False, preview, confirm, no_runs_help,
+                delete_f, confirm_default=not args.permanent)
+        except SystemExit as e:
+            self._reraise_system_exit(e)
+
+    def _reraise_system_exit(self, e):
+        if not e.args[0]:
+            raise e
+        exit_code = e.args[1]
+        msg = e.args[0].replace(
+            "guild runs list",
+            "guild runs list -r %s" % self.name)
+        raise SystemExit(msg, exit_code)
+
+    def _delete_runs(self, runs, permanent):
+        for run in runs:
+            run_uri = self._s3_uri(*(RUNS_PATH + [run.id]))
+            if permanent:
+                self._s3_rm(run_uri)
+            else:
+                deleted_uri = self._s3_uri(*(DELETED_RUNS_PATH + [run.id]))
+                self._s3_mv(run_uri, deleted_uri)
+
+    def _s3_rm(self, uri):
+        rm_args = ["--recursive", uri]
+        self._s3_cmd("rm", rm_args)
+
+    def _s3_mv(self, src, dest):
+        mv_args = ["--recursive", src, dest]
+        self._s3_cmd("mv", mv_args)
+
 def _subprocess_call_to_stderr(cmd):
     p = subprocess.Popen(
         cmd,
@@ -119,10 +179,6 @@ def _subprocess_call_to_stderr(cmd):
         if not line:
             break
         sys.stderr.write(line.decode())
-
-def _s3_path_join(*parts):
-    parts = [part for part in parts if part not in ("/", "")]
-    return "/".join(parts)
 
 def _list(d):
     try:
