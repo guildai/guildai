5e96c14f671a38e283651c429267f14d825b6bfa
Garrett Smith <g@rre.tt>
2018-09-21 19:59:58 -0500

    Missing src

---
+++ guild/test.py
@@ -0,0 +1,249 @@
+# Copyright 2017-2018 TensorHub, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import difflib
+import logging
+import os
+import re
+import subprocess
+import sys
+
+import click
+
+from guild import var
+
+log = logging.getLogger("guild")
+
+class TestError(Exception):
+    pass
+
+class Failed(TestError):
+    pass
+
+class _CompareHelp(object):
+
+    type_attr = "compare-help"
+
+    def __init__(self, config, test):
+        self.compare_to = _test_path(test, config[self.type_attr])
+        self.guildfile_src = test.guildfile.src
+
+    def run(self):
+        _status("Checking help in %s" % os.path.relpath(self.guildfile_src))
+        help_out = _output(["guild", "help", self.guildfile_src])
+        expected = _read(self.compare_to)
+        _compare(help_out, expected, "<help>", self.compare_to)
+
+class _RunOp(object):
+
+    type_attr = "run-op"
+
+    def __init__(self, config, test):
+        self.guildfile = test.guildfile
+        self.op_name = config[self.type_attr]
+        self.no_gpus = config.get("no-gpus", False)
+        self.flag_vals = config.get("flags") or {}
+        self.expect = [
+            _resolve_op_expect(expect, test)
+            for expect in (config.get("expect") or [])]
+
+    def run(self, model=None):
+        self._run_op(model)
+        self._check_expected()
+
+    def _run_op(self, model):
+        op_name = self._op_name(model)
+        _status("Running %s" % op_name)
+        _call(self._run_cmd(op_name), cwd=self.guildfile.dir)
+
+    def _op_name(self, model):
+        if model:
+            return "%s:%s" % (model, self.op_name)
+        else:
+            return self.op_name
+
+    def _run_cmd(self, op_name):
+        cmd = ["guild", "run", "-y", op_name]
+        if self.no_gpus:
+            cmd.append("--no-gpus")
+        for name, val in sorted(self.flag_vals.items()):
+            cmd.append("%s=%s" % (name, val))
+        return cmd
+
+    def _check_expected(self):
+        if not self.expect:
+            return
+        run_dir = _latest_run_dir()
+        _status("Checking %s" % os.path.basename(run_dir))
+        for expected in self.expect:
+            expected.check(run_dir)
+
+class _ForEachModel(object):
+
+    type_attr = "for-each-model"
+
+    def __init__(self, config, test):
+        self.models = self._init_models(config, test)
+        self.steps = [
+            _resolve_model_step(step, test)
+            for step in (config[self.type_attr].get("steps") or [])
+            if not _step_disabled(step)]
+
+    def _init_models(self, config, test):
+        models = config[self.type_attr].get("models")
+        if not models:
+            models = sorted(test.guildfile.models)
+        except_models = set(config[self.type_attr].get("except") or [])
+        return [m for m in models if not m in except_models]
+
+    def run(self):
+        for model in self.models:
+            for step in self.steps:
+                step.run(model=model)
+
+class _ExpectFile(object):
+
+    type_attr = "file"
+
+    def __init__(self, config, test):
+        self.path = config[self.type_attr]
+        self.compare_to = _test_path(test, config.get("compare"))
+
+    def check(self, run_dir):
+        _status("file: %s" % self.path, False)
+        path = os.path.join(run_dir, self.path)
+        if not os.path.isfile(path):
+            raise Failed("%s does not exist" % path)
+        if self.compare_to:
+            _compare_files(path, self.compare_to)
+
+class _ExpectOutput(object):
+
+    type_attr = "output"
+
+    def __init__(self, config, _test):
+        self.pattern = config[self.type_attr]
+        try:
+            self.compiled_pattern = re.compile(self.pattern)
+        except Exception:
+            raise TestError("invalid regular expression: %r" % self.pattern)
+
+    def check(self, run_dir):
+        _status("output: %s" % self.pattern, False)
+        output_path = os.path.join(run_dir, ".guild", "output")
+        try:
+            out = open(output_path, "r").read()
+        except Exception:
+            raise Failed("error reading run output from %s" % output_path)
+        else:
+            if not re.search(self.compiled_pattern, out):
+                raise Failed(
+                    "could not find pattern %r in %s"
+                    % (self.pattern, output_path))
+
+def run_guildfile_test(test):
+    steps = [
+        _resolve_step(step, test) for step in test.steps
+        if not _step_disabled(step)]
+    _status("Testing %s" % test.name)
+    for step in steps:
+        step.run()
+
+def _step_disabled(step):
+    return step.get("disabled") is True
+
+def _resolve_step(step, test, step_classes=None, step_desc="test step"):
+    if step_classes is None:
+        step_classes = [
+            _CompareHelp,
+            _RunOp,
+            _ForEachModel,
+        ]
+    for step_class in step_classes:
+        if step_class.type_attr in step:
+            return step_class(step, test)
+    raise TestError("invalid %s: %r" % (step_desc, step))
+
+def _resolve_model_step(step, test):
+    return _resolve_step(step, test, [_RunOp], "for-each-model step")
+
+def _status(msg, bold=True):
+    click.echo(click.style(msg, bold=bold))
+
+def _test_path(test, path):
+    if path is None:
+        return None
+    return os.path.join(test.guildfile.dir, path)
+
+def _output(cmd):
+    log.debug("cmd: %r", cmd)
+    try:
+        out = subprocess.check_output(cmd)
+    except subprocess.CalledProcessError as e:
+        sys.stdout.write(e.output)
+        sys.stdout.flush()
+        raise Failed("command failed")
+    else:
+        return out.decode()
+
+def _call(cmd, **kw):
+    log.debug("cmd: %r", cmd)
+    try:
+        subprocess.check_call(cmd, **kw)
+    except subprocess.CalledProcessError:
+        raise Failed("command failed")
+
+def _read(path):
+    return open(path, "r").read()
+
+def _compare(actual, expected, actual_src, expected_src):
+    a = re.split(r"\n\r?", expected)
+    b = re.split(r"\n\r?", actual)
+    fromfile = expected_src
+    tofile = actual_src
+    diff = list(difflib.unified_diff(a, b, fromfile, tofile))
+    if diff:
+        sys.stderr.write("\n".join(diff))
+        sys.stderr.write("\n")
+        raise Failed("unexpected file contents")
+
+def _compare_files(actual, expected):
+    _compare(
+        open(actual, "r").read(),
+        open(expected, "r").read(),
+        actual,
+        expected)
+
+def _latest_run_dir():
+    latest = (None, 0)
+    runs_dir = var.runs_dir()
+    for name in os.listdir(runs_dir):
+        path = os.path.join(runs_dir, name)
+        mtime = os.stat(path).st_mtime
+        if mtime > latest[1]:
+            latest = (path, mtime)
+    if latest[0] is None:
+        raise TestError("expected at least one run in %s" % runs_dir)
+    return latest[0]
+
+def _resolve_op_expect(expect, test):
+    expect_classes = [_ExpectFile, _ExpectOutput]
+    for expect_class in expect_classes:
+        if expect_class.type_attr in expect:
+            return expect_class(expect, test)
+    raise TestError("invalid expect config: %r" %  expect)
