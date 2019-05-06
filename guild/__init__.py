023fba27fc95ab75093c2a94e4c044cc8a4f2dfa
Garrett Smith <g@rre.tt>
2019-05-06 17:56:16 -0500

    0.6.2.dev2

--- guild/__init__.py
+++ guild/__init__.py
@@ -18,7 +18,7 @@ from __future__ import division
 import os
 import subprocess
 
-__version__ = "0.6.2.dev1"
+__version__ = "0.6.2.dev2"
 
 # Used by bootstrap_main to ensure minimum required set of modules
 # before continuing.
