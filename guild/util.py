e1e917db61eb9b7393985227323254c07094e94a
Garrett Smith <g@rre.tt>
2019-05-15 10:08:26 -0500

    Don't attempt to read a fifo on is text check (blocks)

--- guild/util.py
+++ guild/util.py
@@ -24,6 +24,7 @@ import platform
 import re
 import shlex
 import shutil
+import stat
 import struct
 import subprocess
 import sys
@@ -498,10 +499,13 @@ else:
     _printable_ascii = _control_chars + bytes(range(32, 127))
     _printable_high_ascii = bytes(range(127, 256))
 
+def is_fifo(path):
+    return stat.S_ISFIFO(os.stat(path).st_mode)
+
 def is_text_file(path, ignore_ext=False):
     # Adapted from https://github.com/audreyr/binaryornot under the
     # BSD 3-clause License
-    if os.path.isdir(path):
+    if os.path.isdir(path) or is_fifo(path):
         return False
     if not ignore_ext:
         ext = os.path.splitext(path)[1].lower()
