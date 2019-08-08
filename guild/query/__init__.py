729dbb2f48def1d6f8c62f896fb7f5d9c490279b
Garrett Smith <g@rre.tt>
2019-08-08 08:57:53 -0500

    Time range support wip

--- guild/query/__init__.py
+++ guild/query/__init__.py
@@ -12,6 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from __future__ import absolute_import
+from __future__ import division
+
 from six.moves import shlex_quote as q
 
 class ParseError(Exception):
