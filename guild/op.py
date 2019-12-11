d1c0135ef9cff1e11ea7bf92d65566980c819037
Garrett Smith <g@rre.tt>
2019-12-10 18:38:33 -0600

    Revert output sync
    The condition these changes are trying to address is highly
    intermittent and shows up in batch output where, on occasion, trial
    output is interleaved.
    
    It's noteworthly that the interleaving is by line. Lines are not
    mixed.
    
    Going to address this by working around the breaking
    test (batch-skopt.md:184 as of this commit)

--- guild/op.py
+++ guild/op.py
@@ -233,14 +233,10 @@ class _RunOutput(object):
         self._output = None
 
 def _proc_wait(proc, stop_after):
-    try:
-        if stop_after is None:
-            return proc.wait()
-        else:
-            return op_util.wait_for_proc(proc, stop_after)
-    finally:
-        assert proc.stdout
-        proc.stdout.flush()
+    if stop_after is None:
+        return proc.wait()
+    else:
+        return op_util.wait_for_proc(proc, stop_after)
 
 def _handle_proc_interrupt(proc):
     log.info("Operation interrupted - waiting for process to exit")
