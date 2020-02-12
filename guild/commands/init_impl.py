c07244745938c6c27420fa4217c7d2cafba47ac5
Garrett Smith <g@rre.tt>
2020-02-11 19:16:20 -0600

    More chasing platform issues
    - Use prefix to insist where guild and reqs are installed (maxOS
      Python 2.7 somehow wants to install in both the venv and the system)
    
    - Use PATH env - work-around macOS Python 2.7 platform specific issue

--- guild/commands/init_impl.py
+++ guild/commands/init_impl.py
@@ -351,7 +351,7 @@ def _install_guild_dist(config):
     # install the Guild req if it's already installed outside the
     # virtual env. The --ignore-installed flag in this case forces pip
     # to actually install Guild.
-    _install_reqs([req], config, ignore_installed=True)
+    _install_reqs([req], config, ignore_installed=True, prefix=config.env_dir)
 
 
 def _guild_reqs_file():
@@ -396,10 +396,16 @@ def _pip_extra_opts(config):
         return []
 
 
-def _install_reqs(reqs, config, ignore_installed=False):
-    cmd_args = _pip_bin_args(config.env_dir) + ["install"] + _pip_extra_opts(config)
+def _install_reqs(reqs, config, ignore_installed=False, prefix=None):
+    cmd_args = (
+        _pip_bin_args(config.env_dir)
+        + ["install", "--no-warn-script-location"]
+        + _pip_extra_opts(config)
+    )
     if ignore_installed:
         cmd_args.append("--ignore-installed")
+    if prefix:
+        cmd_args.extend(["--prefix", prefix])
     for req in reqs:
         if _is_requirements_file(req):
             cmd_args.extend(["-r", req])
@@ -427,8 +433,12 @@ def _check_call_with_empty_env(args):
     Setting the command env to an empty dict corrects this problem and
     ensures that pip installed packages use the correct Python
     path. Note that setting env to None does not correct the problem.
+
+    Update: macOS Python 2.7 has issues when PATH is not in the env so
+    we set it to the value of the current PATH env.
     """
-    subprocess.check_call(args, env={})
+    env = {"PATH": os.getenv("PATH")}
+    subprocess.check_call(args, env=env)
 
 
 def _is_requirements_file(path):
