9caa30caac989686d5a8fac84c92da99a52f0f72
Garrett Smith <g@rre.tt>
2020-07-28 19:00:35 -0500

    Include checkpoint in Keras example

--- examples/keras/mnist_mlp.py
+++ examples/keras/mnist_mlp.py
@@ -46,11 +46,16 @@ model.compile(loss='categorical_crossentropy',
               optimizer=RMSprop(),
               metrics=['accuracy'])
 
+checkpoints = keras.callbacks.ModelCheckpoint(
+    filepath="weights.{epoch:02d}-{val_accuracy:.4f}.hdf5",
+    save_weights_only=True)
+
 history = model.fit(x_train, y_train,
                     batch_size=batch_size,
                     epochs=epochs,
                     verbose=1,
-                    validation_data=(x_test, y_test))
+                    validation_data=(x_test, y_test),
+                    callbacks=[checkpoints])
 score = model.evaluate(x_test, y_test, verbose=0)
 print('Test loss:', score[0])
 print('Test accuracy:', score[1])
