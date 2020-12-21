7a6ed51ec19d674e67551a559697561f86d964b0
Garrett Smith <garrett@placeon.earth>
2020-12-21 07:19:05 -0600

    Wip gist remote - most functionality, no tests

--- guild/remotes/gist.py
+++ guild/remotes/gist.py
@@ -21,6 +21,7 @@ import shutil
 import subprocess
 import zipfile
 
+from guild import log as loglib
 from guild import remote as remotelib
 from guild import remote_util
 from guild import util
@@ -28,6 +29,13 @@ from guild import util
 from . import meta_sync
 
 log = logging.getLogger("guild.remotes.gist")
+
+
+class NoSuchGist(remotelib.OperationError):
+    pass
+
+class MissingRequiredEnv(remotelib.OperationError):
+    pass
 
 
 class GistRemoteType(remotelib.RemoteType):
@@ -51,11 +59,19 @@ class GistRemoteType(remotelib.RemoteType):
 
 def _parse_spec(spec):
     parts = spec.split("/", 1)
-    if len(parts) != 2:
-        raise remotelib.InvalidRemoteSpec(
-            "gist remotes must be specified as USER/GIST_NAME"
-        )
+    if len(parts) == 1:
+        return _required_gist_user_env(), parts[0]
     return parts
+
+
+def _required_gist_user_env():
+    try:
+        return os.environ["GIST_USER"]
+    except KeyError:
+        raise MissingRequiredEnv(
+            "gist remotes must be specified as USER/GIST_NAME if GIST_USER "
+            "environment variable is not defined"
+        )
 
 
 class GistRemote(meta_sync.MetaSyncRemote):
@@ -71,20 +87,31 @@ class GistRemote(meta_sync.MetaSyncRemote):
         super(GistRemote, self).__init__(runs_dir, None)
 
     def _sync_runs_meta(self, force=False):
-        gist = self._repo_gist()
-        if not gist:
-            raise remotelib.OperationError(
-                "cannot find gist remote '%s' (denoted by the file '%s') for user %s"
-                % (self.gist_name, self._gist_readme_name, self.user)
-            )
-        _sync_gist_repo(gist, self._local_gist_repo, self.local_env)
+        self._ensure_local_gist_repo()
         self._sync_runs_meta_for_gist(force)
 
+    def _ensure_local_gist_repo(self):
+        if _is_git_repo(self._local_gist_repo):
+            log.debug("gist local repo found at %s", self._local_gist_repo)
+            return
+        log.debug("initializing gist local repo at %s", self._local_gist_repo)
+        gist = self._repo_gist()
+        _sync_gist_repo(gist, self._local_gist_repo, self.local_env)
+
     def _repo_gist(self):
-        return _find_gist_with_file(self.user, self._gist_readme_name)
+        gist = _find_gist_with_file(self.user, self._gist_readme_name)
+        if not gist:
+            raise NoSuchGist(
+                "cannot find gist remote '%s' (denoted by the file '%s') for %s\n"
+                "If the gist is private, you must specify a valid access token with "
+                "GIST_ACCESS_TOKEN.\nFor more information see "
+                "https://my.guild.ai/docs/gists."
+                % (self.gist_name, self._gist_readme_name, self.user)
+            )
+        return gist
 
     def _sync_runs_meta_for_gist(self, force):
-        git_commit = self._gist_repo_commit()
+        git_commit = self._gist_repo_current_commit()
         if not force and self._meta_current(git_commit):
             return
         _refresh_runs_meta(
@@ -97,8 +124,8 @@ class GistRemote(meta_sync.MetaSyncRemote):
     def _meta_current(self, git_commit):
         return meta_sync.meta_current(self.local_sync_dir, lambda: git_commit)
 
-    def _gist_repo_commit(self):
-        return _git_commit(self._local_gist_repo)
+    def _gist_repo_current_commit(self):
+        return _git_current_commit(self._local_gist_repo)
 
     def _delete_runs(self, runs, permanent):
         assert False
@@ -110,17 +137,33 @@ class GistRemote(meta_sync.MetaSyncRemote):
         assert False
 
     def push(self, runs, delete=False):
-        for run in runs:
-            self._push_run(run, delete)
-            self._new_meta_id()
-        self._sync_runs_meta(force=True)
+        log.info(loglib.dim("Synchronizing runs with %s"), self.name)
+        self._ensure_synced_gist_repo()
+        _export_runs_to_gist_archives(runs, self._local_gist_repo)
+        _commit_and_push_gist_repo(
+            self._local_gist_repo,
+            _push_commit_msg(),
+            self.local_env,
+        )
+        self._sync_runs_meta_for_gist(True)
 
-    def _push_run(self, run, delete):
-        assert False, (run, self._gist_readme_name, delete)
+    def _ensure_synced_gist_repo(self):
+        try:
+            self._sync_runs_meta()
+        except NoSuchGist:
+            self._init_gist_repo()
+
+    def _init_gist_repo(self):
+        gist = _create_gist(self.user, self.gist_name, self._gist_readme_name)
+        _sync_gist_repo(gist, self._local_gist_repo, self.local_env)
 
     def pull(self, runs, delete=False):
         from guild import var
 
+        # That we have `runs` means we've sync'd runs meta. "Meta" in
+        # this case also contains the runs themselves as zip
+        # archives. At this point we need only extract the run
+        # archives to the runs dir.
         _extract_runs(runs, self._local_gist_repo, var.runs_dir())
 
     def label_runs(self, **opts):
@@ -185,7 +228,11 @@ def _find_gist_with_file(user, filename):
     page = 1
     url = "https://api.github.com/users/%s/gists" % user
     while True:
-        resp = requests.get(url, params={"page": page, "per_page": 100})
+        resp = requests.get(
+            url,
+            params={"page": page, "per_page": 100},
+            headers=_github_auth_headers(),
+        )
         gists = resp.json()
         if not gists:
             return None
@@ -194,6 +241,15 @@ def _find_gist_with_file(user, filename):
                 if name == filename:
                     return gist
         page += 1
+
+
+def _github_auth_headers():
+    try:
+        access_token = _required_gist_access_token()
+    except MissingRequiredEnv:
+        return {}
+    else:
+        return {"Authorization": "token %s" % access_token}
 
 
 def _sync_gist_repo(gist, local_repo, env):
@@ -240,8 +296,12 @@ def _refresh_runs_meta(gist_repo, runs_dir, meta_id, local_sync_dir):
 
 def _run_archives(dir):
     for name in os.listdir(dir):
-        if name.endswith(".run.zip"):
+        if _is_guild_run(name):
             yield os.path.join(dir, name)
+
+
+def _is_guild_run(name):
+    return name.startswith("guildai-run-") and name.endswith(".zip")
 
 
 def _unpack_meta(archive, runs_dir):
@@ -260,7 +320,11 @@ def _is_meta_file(name):
     )
 
 
-def _git_commit(git_repo):
+def _is_git_repo(dir):
+    return os.path.exists(os.path.join(dir, ".git"))
+
+
+def _git_current_commit(git_repo):
     if not os.path.exists(git_repo):
         return None
     cmd = [_git_cmd(), "--work-tree", git_repo, "log", "-1", "--format=%H"]
@@ -271,7 +335,7 @@ def _git_commit(git_repo):
 
 def _extract_runs(runs, archive_dir, dest_dir):
     for run in runs:
-        archive = os.path.join(archive_dir, "%s.run.zip" % run.id)
+        archive = os.path.join(archive_dir, _run_archive_filename(run.id))
         if not os.path.exists(archive):
             log.error(
                 "%s archive for gist does not exist (%s), skipping", run.id, archive
@@ -279,6 +343,10 @@ def _extract_runs(runs, archive_dir, dest_dir):
             continue
         log.info("Copying %s", run.id)
         _replace_run(archive, run.id, dest_dir)
+
+
+def _run_archive_filename(run_id):
+    return "guildai-run-%s.zip" % run_id
 
 
 def _replace_run(archive, run_id, dest_dir):
@@ -309,3 +377,107 @@ def _replace_run_dir(run_dir, src_dir):
     log.debug("moving %s to %s", src_dir, run_dir)
     util.ensure_safe_rmtree(run_dir)
     shutil.move(src_dir, run_dir)
+
+
+def _create_gist(user, gist_remote_name, gist_readme_name):
+    import requests
+
+    access_token = _required_gist_access_token()
+    data = {
+        "accept": "application/vnd.github.v3+json",
+        "description": "Guild AI Repository",
+        "public": True,
+        "files": {
+            gist_readme_name: {
+                "filename": gist_readme_name,
+                "type": "text/markdown",
+                "language": "Markdown",
+                "content": _gist_readme_content(gist_remote_name),
+            }
+        },
+    }
+    headers = {
+        "Authorization": "token %s" % access_token,
+    }
+    resp = requests.post("https://api.github.com/gists", json=data, headers=headers)
+    if resp.status_code not in (200, 201):
+        raise OperationError(
+            "error creating gist: (%i) %s" % (resp.status_code, resp.text)
+        )
+    return resp.json()
+
+
+def _required_gist_access_token():
+    try:
+        return os.environ["GIST_ACCESS_TOKEN"]
+    except KeyError:
+        raise MissingRequiredEnv(
+            "missing required environment variable GIST_ACCESS_TOKEN\n"
+            "This operation requires a GitHub personal access token for "
+            "creating gists.\n"
+            "See https://my.guild.ai/docs/gists for more information."
+        )
+
+
+def _gist_readme_content(remote_name):
+    return (
+        "This is a Guild AI runs repository. To access runs, "
+        "[install Guild AI](https://guild.ai/install) and run `guild pull gist:%s`. "
+        "For more information about Guild AI Gist based repositories, see "
+        "[Guild AI - Gists](https://my.guild.ai/docs/gist)." % remote_name
+    )
+
+
+def _export_runs_to_gist_archives(runs, gist_repo):
+    with util.TempDir("guild-runs-export-") as tmp:
+        archives = [_run_export_archive(run, tmp.path) for run in runs]
+        _export_runs(zip(runs, archives))
+        for archive_src in archives:
+            archive_dest = os.path.join(gist_repo, os.path.basename(archive_src))
+            util.ensure_deleted(archive_dest)
+            shutil.move(archive_src, archive_dest)
+
+
+def _run_export_archive(run, export_dir):
+    return os.path.join(export_dir, _run_archive_filename(run.id))
+
+
+def _export_runs(runs_with_dest):
+    from guild import run_util
+
+    for run, dest in runs_with_dest:
+        run_util.export_runs([run], dest, copy_resources=False)
+
+
+def _push_commit_msg():
+    import guild
+
+    return "`guild push` by %s@%s with version %s" % (
+        util.user(),
+        util.hostname(),
+        guild.version(),
+    )
+
+
+def _commit_and_push_gist_repo(repo, commit_msg, env):
+    _git_add_all(repo, env)
+    _git_commit(repo, commit_msg, env)
+    _git_push(repo, env)
+
+
+def _git_add_all(repo, env):
+    cmd = [_git_cmd(), "-C", repo, "add", "."]
+    log.debug("git cmd: %s", cmd)
+    remote_util.subprocess_call(cmd, extra_env=env, quiet=True)
+
+
+def _git_commit(repo, msg, env):
+    cmd = [_git_cmd(), "-C", repo, "commit", "-m", msg]
+    log.debug("git cmd: %s", cmd)
+    remote_util.subprocess_call(cmd, extra_env=env, quiet=True)
+
+
+def _git_push(repo, env):
+    cmd = [_git_cmd(), "-C", repo, "push"]
+    log.debug("git cmd: %s", cmd)
+    remote_util.subprocess_call(cmd, extra_env=env, quiet=True)
