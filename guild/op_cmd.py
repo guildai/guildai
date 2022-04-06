351c32b506e33c763448d6bd2a6be314daabd648
Michael Sarahan <msarahan@gmail.com>
2022-04-06 14:23:35 -0500

    ignore batches of binary flags on first pass

--- guild/op_cmd.py
+++ guild/op_cmd.py
@@ -111,7 +111,8 @@ def _args_for_flag(name, val, cmd_flag, flag_dest, cmd_args):
     elif cmd_flag.arg_switch is not None:
         # does the specified value match the default value? If not, pass the
         #    flag to toggle the state to opposite of the default
-        if cmd_flag.arg_switch != val:
+        # Note that we ignore lists on the first pass, before computing batches.
+        if cmd_flag.arg_switch != val and not isinstance(val, list):
             return ["--%s" % arg_name]
         else:
             return []
