de96aba69c2ea193bf7f6c932443d23c8607ab90
Andy Kipp <anki-code@users.noreply.github.com>
2023-03-07 23:40:16 +0600

    Update util.py

--- guild/util.py
+++ guild/util.py
@@ -1795,13 +1795,14 @@ class lazy_str:
 
 _KNOWN_SHELLS = (
     "bash",
-    "zsh",
-    "fish",
-    "dash",
-    "sh",
     "cmd",
+    "dash",
+    "fish",
     "powershell",
     "pwsh",
+    "sh",
+    "xonsh",
+    "zsh",
 )
 _cached_active_shell = "__unset__"
 
